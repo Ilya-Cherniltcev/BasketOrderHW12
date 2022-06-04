fb744fe9af743a50debadb627b88033ee4059f99
Ilya Cherniltsev <cherniltcev@mail.ru>
2022-06-04 23:17:07 +0500

    Initial commit

---
+++ src/main/java/pro/sky/course2/spring/basketorderhw12/BasketOrderHw12Application.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.spring.basketorderhw12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketOrderHw12Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketOrderHw12Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/course2/spring/basketorderhw12/BasketOrderHw12ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.course2.spring.basketorderhw12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketOrderHw12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
